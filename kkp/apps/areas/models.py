edc8ceed049804a2dc1786a2ff7e6b45b8d5a9e3
binotby <binotby@gmail.com>
2020-08-14 22:39:03 +0700

    setup project

---
+++ kkp/apps/areas/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Province(models.Model):
+    # Provinsi in Indonesia
+    pass
+
+
+class District(models.Model):
+    # Kabupaten in Indonesia
+    pass
+
+
+class SubDistrict(models.Model):
+    # Kecamatan in Indonesia
+    pass
+
+
+class UrbanVillage(models.Model):
+    # Kelurahan in Indonesia
+    pass
+
+
+class Hamlet(models.Model):
+    # Rukun Warga in Indonesia
+    pass
+
+
+class Neighbourhood(models.Model):
+    # Rukun Tetangga in Indonesia
+    pass
